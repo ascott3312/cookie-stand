12bdfe3b648cdbc378c36207c9e779f8f7c7988f
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-23 16:10:10 -0500

    Finished Store One

--- js/app.js
+++ js/app.js
@@ -1,62 +1,50 @@
 'use strict';
 console.log('app.js is connected.');
 
-//Stores the min/max hourly customers,and the average cookies per customer, in object properties
-
-var Seattle = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookiesale: 6.3,
-  custperhr: 0, //Random customer per hour
-  setCustperhr: function () {
-    this.custperhr=randomCustperhr(this.minCust,this.maxCust); //Random customer per hour
-  },
-  cookiespurch: 0, //calc cookies purchased per hour
-  setCookiesperhr: function () {
-    this.cookiespurch=multiply(this.avgCookiesale,this.custperhr);
-  },
-  hoursOperation: ['6am:',this.cookiesperhr, 'cookies',
-    '7am:',this.cookiesperhr,'cookies',
-    '8am:',this.cookiesperhr,'cookies',
-    '9am:',this.cookiesperhr,'cookies',
-    '10am:',this.cookiesperhr,'cookies',
-    '11am:',this.cookiesperhr,'cookies',
-    '12pm:',this.cookiesperhr,'cookies',
-    '1pm:',this.cookiesperhr,'cookies',
-    '2pm:',this.cookiesperhr,'cookies',
-    '3pm:',this.cookiesperhr,'cookies',
-    '4pm:',this.cookiesperhr,'cookies',
-    '5pm:',this.cookiesperhr,'cookies',
-    '6pm:',this.coookiesperhr,'cookies',
-    '7pm:',this.cookiesperhr,'cookies',
-  ],
-}
-Seattle.setCustperhr();
-Seattle.setCookiesperhr();
-console.log(Seattle);
-
-//Uses a method of that object to generate a random number of customers per hour
-
+var hoursOperation = ['6am:','7am:','8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:','6pm:','7pm:'];
 function randomCustperhr(min,max) {
   return Math.floor(Math.random() * (max - min + 1 ) + min);
 }
-randomCustperhr();
 
-//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+var storeOne = {
+  name:'Seattle',
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesale: 6.3,
+  customerPerHour: [],
+  custPerHr: function() {
+    for (var i = 0; i <hoursOperation.length; i++) {
+      this.customerPerHour.push(randomCustperhr(this.minCust, this.maxCust));
+    }
+  },
+  cookiesPerHour: [],
+  cookiesPerHr: function() {
+    this.custPerHr();
+    for (var i = 0; i <this.customerPerHour.length; i++) {
+      this.cookiesPerHour.push(Math.floor(this.customerPerHour[i]*this.avgCookiesale));
+    }
+  },
+  totalCookies: 0,
+  calcTotal: function() {
+    this.cookiesPerHr();
+    for (var i = 0; i <this.cookiesPerHour.length; i++) {
+      this.totalCookies = this.totalCookies + this.cookiesPerHour[i];
+    }
+  }
+};
+console.log (storeOne);
 
-function multiply(a, b) {
-  var product = a * b;
-  return Math.round(product);
+storeOne.calcTotal();
+console.log(storeOne.totalCookies);
+
+var root = document.getElementById('root');
+var ul1 = document.createElement('ul');
+root.appendChild(ul1);
+for (var i = 0; i <storeOne.cookiesPerHour.length; i++) {
+  var li = document.createElement('li');
+  li.textContent = `${hoursOperation[i]} ${storeOne.cookiesPerHour[i]} cookies`;
+  ul1.appendChild(li);
 }
-multiply();
-
-// Store the results for the array
-
-// function cookiesPerOper () {
-        // var = hoursOperation
-        // var = arrayLength = hoursOperation.length;
-//     for (var i = 0; i <= 13;)(i++); {
-//         if i <= 
-
-//     }
-// }
+var total = document.createElement('li');
+total.textContent = `Total: ${storeOne.totalCookies}`;
+ul1.appendChild(total);
