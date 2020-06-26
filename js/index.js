108b79b5cb596bf094d488749098c2efcd1080f8
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-26 09:00:17 -0500

    New updates for lab 09

--- js/index.js
+++ js/index.js
@@ -1,21 +1,26 @@
 //display a new store that the user inputs
-
 function handleFormSubmitted(event) {
-
+//console.log('form button was clicked');
+//console.log(event);
   event.preventDefault();
 
   var nameInput = document.getElementById('name');
   var nameValue = nameInput('value');
+
   var minInput = document.getElementById('minCust');
   var minNumber = minInput.value;
+
   var maxInput = document.getElementById('maxCust');
   var maxNumber = maxInput.value;
+
   var avgCust = document.getElementById('avgCookiesale');
   var avgNumber = avgCust.value;
 
-  var newStore = newStore(nameValue, minNumber, maxNumber, avgNumber);
+  var newStore = new Store(nameValue, minNumber, maxNumber, avgNumber);
 
   var form = document.getElementById(newStore);
-  form.requestFullscreen();
+  form.reset();
 }
-handleFormSubmitted();
+
+var formElement = document.getElementById('newStore');
+formElement.addEventListener('submit', handleFormSubmitted);
